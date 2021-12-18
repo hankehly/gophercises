9ae7ef6d5d1fbbd7f5bd66d04cfad6f75e8df650
hankehly <henry.ehly@gmail.com>
2021-12-18 12:42:52 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gophercises/m/v2
+
+go 1.17
---
+++ quiz/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("problems.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.ReuseRecord = true
+
+	var answer, n_correct, n_total int
+
+	for {
+		record, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		expected, err := strconv.Atoi(record[1])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n_total++
+
+		fmt.Printf("%s ", record[0])
+		fmt.Scan(&answer)
+
+		if answer == expected {
+			n_correct++
+		}
+	}
+
+	score_pct := float64(n_correct) / float64(n_total) * 100
+	fmt.Printf("Score: %d/%d (%.2f%%)\n", n_correct, n_total, score_pct)
+}
