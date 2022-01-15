47ce765f2a363e33577afa58f5be915bb147d269
hankehly <henry.ehly@gmail.com>
2022-01-15 10:52:19 -0600

    Use function options to configure handler

--- cyoa/cyoa.go
+++ cyoa/cyoa.go
@@ -2,31 +2,13 @@ package cyoa
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"io"
+	"log"
+	"net/http"
+	"strings"
 )
-
-var Template string = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Choose Your Own Adventure - {{ .Title }}</title>
-</head>
-<body>
-    <h1>{{ .Title }}</h1>
-    {{ range .Story }}
-    <p>{{ . }}</p>
-    {{ end }}
-    <ul>
-        {{ range .Options }}
-        <li>
-            <a href="/{{ .Arc }}">{{ .Text }}</a>
-        </li>
-        {{ end }}
-    </ul>
-</body>
-</html>
-`
 
 func JsonStory(r io.Reader) (Story, error) {
 	var story Story
@@ -39,13 +21,99 @@ func JsonStory(r io.Reader) (Story, error) {
 
 type Story map[string]Chapter
 
-type Option struct {
+type ChapterOption struct {
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
 }
 
 type Chapter struct {
-	Title   string   `json:"title"`
-	Story   []string `json:"story"`
-	Options []Option `json:"options"`
+	Title          string          `json:"title"`
+	Story          []string        `json:"story"`
+	ChapterOptions []ChapterOption `json:"options"`
+}
+
+// Isn't this null? Why are we using it as a default?
+var tpl *template.Template
+
+var defaultTemplate string = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose Your Own Adventure - {{ .Title }}</title>
+</head>
+<body>
+    <h1>{{ .Title }}</h1>
+    {{ range .Story }}
+    <p>{{ . }}</p>
+    {{ end }}
+    <ul>
+        {{ range .ChapterOptions }}
+        <li>
+            <a href="/{{ .Arc }}">{{ .Text }}</a>
+        </li>
+        {{ end }}
+    </ul>
+</body>
+</html>
+`
+
+// Using init, we can validate our HTML template before execution
+// We initialize the validated tpl value here so it can be used elsewhere
+func init() {
+	log.Println("init called")
+	tpl = template.Must(template.New("").Parse(defaultTemplate))
+}
+
+type HandlerOption func(h *handler)
+
+func WithTemplate(tmpl template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = tmpl
+	}
+}
+
+// An example of "Function Options" for configuration
+// https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	// Initialize your value with defaults
+	h := handler{s, *tpl}
+	// Allow the programmer to modify the handler using any number of
+	// HandlerOption functions
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
+}
+
+// handler is an http.Handler because it implements ServeHTTP
+// but we can add other properties to it
+type handler struct {
+	s Story
+	t template.Template
+}
+
+func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// Note:
+	// Argument to New must be base name of template file (if using a file)
+	// https://pkg.go.dev/text/template@go1.17.6#Template.ParseFiles
+	// pathTemplate := "/Users/hankehly/Projects/gophercises/cyoa/template.html"
+	// baseName := filepath.Base(pathTemplate)
+	// tmpl, err := template.New(baseName).ParseFiles(pathTemplate)
+	//
+	path := strings.TrimSpace(r.URL.Path)
+	parts := strings.Split(path, "/")
+	key := parts[len(parts)-1]
+	if key == "" {
+		key = "intro"
+	}
+	if chapter, ok := h.s[key]; ok {
+		err := h.t.Execute(rw, chapter)
+		if err != nil {
+			fmt.Printf("%v", err)
+			http.Error(rw, fmt.Sprintln("Internal server error"), http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(rw, "Invalid chapter name", http.StatusNotFound)
 }
--- cyoa/web/main.go
+++ cyoa/web/main.go
@@ -3,61 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"gophercises/m/v2/cyoa"
 )
 
 func main() {
+	// Todo: What is a more elegant way of handling null user input?
+	unset := "UNSET"
+
 	port := flag.Int("port", 3030, "CYOA web app port")
+	jsonStoryPath := flag.String("jsonStoryPath", unset, "Path to JSON story")
+
 	flag.Parse()
-	http.HandleFunc("/", index)
 
-	fmt.Printf("Started the web application on http://localhost:%d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
-}
-
-func index(rw http.ResponseWriter, r *http.Request) {
-	storyPath := "/Users/hankehly/Projects/gophercises/cyoa/story.json"
-
-	f, err := os.Open(storyPath)
+	if *jsonStoryPath == unset {
+		log.Fatalln("jsonStoryPath is required")
+	}
+	f, err := os.Open(*jsonStoryPath)
 	if err != nil {
-		fmt.Printf("%v", err)
-		http.Error(rw, fmt.Sprintln("Internal server error"), http.StatusInternalServerError)
+		log.Fatal(err)
 	}
-
-	chapters, err := cyoa.JsonStory(f)
+	story, err := cyoa.JsonStory(f)
 	if err != nil {
-		fmt.Printf("%v", err)
-		http.Error(rw, fmt.Sprintln("Internal server error"), http.StatusInternalServerError)
+		log.Fatal(err)
 	}
 
-	// Argument to New must be base name of template file (if using a file)
-	// https://pkg.go.dev/text/template@go1.17.6#Template.ParseFiles
-	// pathTemplate := "/Users/hankehly/Projects/gophercises/cyoa/template.html"
-	// baseName := filepath.Base(pathTemplate)
-	// tmpl, err := template.New(baseName).ParseFiles(pathTemplate)
+	// Set a custom template using function options like this
+	// tmpl := template.Must(template.New("notImportant").Parse("hello world"))
+	// setTemplate := cyoa.WithTemplate(*tmpl)
+	// h := cyoa.NewHandler(story, setTemplate)
+	//
+	// Or use default values (set inside NewHandler)
+	h := cyoa.NewHandler(story)
 
-	tmpl := template.Must(template.New("doesNotMatter").Parse(cyoa.Template))
-
-	path := strings.TrimSpace(r.URL.Path)
-	parts := strings.Split(path, "/")
-	key := parts[len(parts)-1]
-	if key == "" {
-		key = "intro"
-	}
-
-	if chapter, ok := chapters[key]; ok {
-		err = tmpl.Execute(rw, chapter)
-		if err != nil {
-			fmt.Printf("%v", err)
-			http.Error(rw, fmt.Sprintln("Internal server error"), http.StatusInternalServerError)
-		}
-		return
-	}
-	http.Error(rw, "Invalid chapter name", http.StatusNotFound)
+	addr := fmt.Sprintf("0.0.0.0:%d", *port)
+	log.Printf("Started the web application on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, h))
 }
